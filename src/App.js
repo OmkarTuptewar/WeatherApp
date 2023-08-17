9b877d1043e563308a04727632c5d839142945db
OmkarTuptewar <omkarsanjay2004@gmail.com>
2023-08-17 23:34:47 +0530

    created the weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Weather from "./components/weather/weather"
+
+const App=()=>{
+
+    return(
+        <>
+        <Weather/>
+        
+        </>
+    )
+}
+
+export default App
